00290ab5332ebfc38406c529fffc8f061569fbf4
mahaprasad.nayak <mahaprasad.n@iserveu.in>
2023-06-07 11:25:33 +0530

    added to file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+ import Style from "./App.css";
+import blogPost from './countryTime.json';
+import Timezone from './timeZone';
+
+function App() {
+  return (
+    <Timezone></Timezone>
+  );
+}
+
+export default App;
